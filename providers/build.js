f23962f1436b115efa50ae32060bdc87a0928577
hassanamin95600 <hassanamin95600@gmail.com>
2026-08-17 14:42:04 +0200

    Delete providers/build.js

--- providers/build.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-
-console.log('Building providers...');
-if (!fs.existsSync(path.join(__dirname, 'providers'))) {
-  fs.mkdirSync(path.join(__dirname, 'providers'));
-}
-console.log('Build completed successfully.');
